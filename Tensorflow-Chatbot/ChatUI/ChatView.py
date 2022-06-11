10115897ea2c80cf91b1598bdbc0f2a35b3ab4c8
hoanguit <luna02091999@gmail.com>
2022-06-11 12:44:16 +0700

    [update] My chatbot with django

---
+++ Tensorflow-Chatbot/ChatUI/ChatView.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+import tkinter
+import pygubu
+import tkinter.messagebox
+
+class Application:
+    def __init__(self,master):
+        self.master = master
+        self.builder = builder = pygubu.Builder()
+        builder.add_from_file('chat_window.ui')
+        self.mainWindow = builder.get_object('mainwindow', master)
+
+        self.textView = builder.get_object('Entry_1',master)
+        builder.connect_callbacks(self)
+
+
+
+
+    def onQuit(self):
+        print(self.textView.get())
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = Application(root)
+    root.mainloop()
